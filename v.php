5bddbb24142a7a5a09a504c6badba83dc6ee48a4
hemantpar007 <100411751+hemantpar007@users.noreply.github.com>
2022-03-26 16:42:17 +0530

    Update v.php

--- v.php
+++ v.php
@@ -1,7 +1,23 @@
-!DOCTYPE html>
-<html>
-<body>            
-    <h1>My first PHP page</h1><br>
-    <?php print "Hello World!"; ?>
-</body>
-</html>
+<?php
+
+// Displaying string of text
+print "Hello World!";
+echo "<br>";
+
+// Displaying HTML code
+print "<h4>This is a simple heading.</h4>";
+print "<br>";
+print "<h4 style='color: red;'>This is heading with style.</h4>";
+print "<br>";
+
+$myTxt = "Hello World!";
+$myNum = 123456789;
+$myColors = array("Red", "Green", "Blue");
+ 
+// Displaying variables
+print $myTxt;
+print "<br>";
+print $myNum;
+print "<br>";
+print $myColors[0];
+print "<br>";
